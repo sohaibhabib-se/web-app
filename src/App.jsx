530b079c5b7ef6f9b757e1470edc2b09cade63e2
Sohaib Habib <sohaibhabib8@gmail.com>
2024-07-03 00:04:08 +0500

    Lecture 10 in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from 'styled-components'
 
 function App() {
   const storageKeyName = "count";
