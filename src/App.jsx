5eb513ff02683cbc729ea9783b089acce51ffe09
Sohaib Habib <sohaibhabib8@gmail.com>
2024-07-02 19:49:11 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const storageKeyName = "count";
+  // const storedCount = localStorage.getItem(storageKeyName);
+  // const retrieveCountValue = () => Number( storedCount ? storedCount : 0);
+  const retrieveCountValue = () => Number( localStorage.getItem(storageKeyName) || 0);
+  const [count, setCount] = useState(retrieveCountValue());
+  const addNumber = (count) => setCount(Number(count) + 1);
+
+  useEffect(() => {
+    localStorage.setItem(storageKeyName, String(count));
+  }, [count]);
+
+  return (
+    <>
+    Hello
+    <button onClick={() => addNumber(count)}>
+      count is {count}
+    </button>
+    </>
+  )
+}
+
+export default App
